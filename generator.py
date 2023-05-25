ccfe7767d179e4b22653cd09adba980b6ddf6e14
Ehsan <e.galavi1995@gmail.com>
2023-05-25 18:49:24 +0330

    define main function

--- generator.py
+++ generator.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
-import csv
-from random import Random
+import os
+import sys
 import multiprocessing
+from random import Random
+
+dirname = "example"
 
 
 def generate_input(i: int) -> None:
-    with open(f'input{i}.csv', 'w') as csvfile:
+    filename = os.path.join(dirname, f"input{i}.csv")
+    with open(filename, 'w') as csvfile:
         csvfile.write("id,duration\n")
         rng = Random(i)
         for _ in range(0, 100_000_000):
@@ -15,15 +19,29 @@ def generate_input(i: int) -> None:
             csvfile.write(f"{id},{duration}\n")
 
 
-pool = multiprocessing.Pool()
-pool.map(generate_input, range(0, 10))
+def generate_id() -> None:
+    filename = os.path.join(dirname, 'ids.csv')
+    with open(filename, 'w') as csvfile:
+        csvfile.write("id\n")
+        ids = set()
+        rng = Random(99)
+        while (len(ids) != 5_000_000):
+            ids.add(10_000_000+rng.randint(0, 9_999_999))
 
-with open('ids.csv', 'w') as csvfile:
-    csvfile.write("id\n")
-    ids = set()
-    rng = Random(99)
-    while (len(ids) != 5_000_000):
-        ids.add(10_000_000+rng.randint(0, 9_999_999))
+        for id in ids:
+            csvfile.write(f"{id}\n")
 
-    for id in ids:
-        csvfile.write(f"{id}\n")
+
+def main(argc, argv):
+    if argc == 1:
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        pool = multiprocessing.Pool()
+        pool.map(generate_input, range(0, 10))
+
+        generate_id()
+
+
+if __name__ == "__main__":
+    main(len(sys.argv), sys.argv)
