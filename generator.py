5279c3046e9c8b74bb18759a445d033f7c851d52
Ehsan <e.galavi1995@gmail.com>
2023-05-25 17:47:33 +0330

    create generator

---
+++ generator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import csv
+from random import Random
+import multiprocessing
+
+
+def generate_input(i: int) -> None:
+    with open('input{}.csv'.format(i), 'w') as csvfile:
+        csvfile.write("id,duration\n")
+        rng = Random(i)
+        for _ in range(0, 100_000_000):
+            csvfile.write("{},{}\n".format(10_000_000+rng.randint(0, 9_999_999), rng.randint(1, 10000)))
+
+
+pool = multiprocessing.Pool()
+pool.map(generate_input, range(0, 10))
+
+with open('ids.csv', 'w') as csvfile:
+    csvfile.write("id\n")
+    ids = set()
+    rng = Random(99)
+    while (len(ids) != 5_000_000):
+        ids.add(10_000_000+rng.randint(0, 9_999_999))
+
+    for id in ids:
+        csvfile.write("{}\n".format(id))
